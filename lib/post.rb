765163dd2469fb4233d4234fd075b84c81fa6111
taryn yaeger <tsyaeger@gmail.com>
2018-05-23 20:25:38 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+
+
+
+class Post 
+  
+  attr_accessor :title, :author
+  
+  author = Author.new 
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+
+
+
+class Song 
+  
+  attr_accessor :title, :artist 
+  
+  @artist = Artist.new 
+  
+end
